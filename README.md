34ec8ce124d7836577338dbec5ddacf30c0b18c0
Tech-wizard <876424077@qq.com>
2017-04-04 14:51:58 +0800

    更新进度

---
+++ README.md
@@ -0,0 +1,14 @@
+# 引擎命令行
+
+## 职责
+* 创建项目
+     *要有模板
+     *拷贝到目标路径
+     *执行方法 engine create {your_project_forder}
+* 编译引擎
+* 编译项目
+
+##如何构建
+
+*检出代码
+*执行npm install
---
+++ src/cli.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import * as create from './create';
+import * as build from './build';
+import * as run from './run';
+let command = process.argv[2];
+console.log(command)
+if (command == "create") {
+    let projectPath = process.argv[3];
+    create.create(projectPath);
+}
+else if (command == "build") {
+    build.buildAll();
+}
+else if (command == "run") {
+    run.run();
+}
